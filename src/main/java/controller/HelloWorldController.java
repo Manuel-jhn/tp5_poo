c2eb773413b15ebacf320822e6b6dfc51f78b522
Manuel Jehanno <manuel.jehanno.etu@univ-lemans.fr>
2019-11-21 16:41:15 +0100

    etape 11
    ecrire dossier templates dans resources

---
+++ src/main/java/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class HelloWorldController {
+
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="nameGET", required=false, defaultValue="World") String nameGET, Model model) {
+		model.addAttribute("nomTemplate", nameGET);
+		return "greeting";
+	}
+}
